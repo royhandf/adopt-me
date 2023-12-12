9a9d96276f23c1c88013857b23474e8f2c1e156e
royhandf <royhandaffa14@gmail.com>
2023-12-12 11:26:40 +0700

    feat: react-query to fetch pet

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,33 @@ import React from "react";
 import SearchParams from "./components/SearchParams";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Details from "./components/Details";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function App() {
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
   return (
     <BrowserRouter>
-      <div className="App">
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/" element={<SearchParams />} />
-        </Routes>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
